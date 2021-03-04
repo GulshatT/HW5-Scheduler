5f9cf72025da844a31572aa2fa1e59a6c965f55c
gigi <guka.tgs@gmail.com>
2021-03-04 01:42:24 -0500

    current time in js

---
+++ script.js
@@ -0,0 +1,36 @@
+$('#currentDay').text(moment().format('MMMM Do YYYY, h:mm:ss a'));
+
+//button 
+// get a reference to all of the save buttons $('.saveBtn')
+//01 - ins _jQuery-elements
+
+// add a click handler to each save Button using .on() method
+//jquery click event
+
+// traverse the DOM to get the values that we need to store to the localStorage
+//jquery traversing .parent() .sibling()
+//setItems in localStorage  /unit 04
+
+
+// save to local storage
+//handle color coding
+// use the moment library to get the current hour - set hour
+
+// get a reference tp all timeblocks 
+//loop throught the timeblocks
+
+// get the data-hour value timeblock [i].data('hour')
+//if , else - condition we compare thr currentHOur wuth the timeBLock <, ===,
+
+// handle color coding
+
+// get the data from localstorage and populate the timeblocks
+//getItem 
+//reference to the textarea id
+//.val()
+//approach $(textarea;) ->loop (parent)-> val()
+
+// current day -> top calendar
+// call the colorCoding function
+// setInterval -> 15000
+// unit4
