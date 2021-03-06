57fed2db98b4ea0eeead9c78d05a2a32c2209f64
gigi <guka.tgs@gmail.com>
2021-03-06 13:37:07 -0500

    updated js and html

--- README.md
+++ README.md
@@ -1,10 +1,17 @@
 To create a simple calendar application that allows the user to save events for each hour of the day (with HTML and CSS powered by jQuery) I need the following:
 
 1.Use the Moment.js library to work with date and time.
+
 2.The current day is displayed at the top of the calendar.
-3.By scrolling down user is presented with timeblocks for standard business hours (I took from 8am to 5pm).
+
+3.By scrolling down user is presented with timeblocks for standard business hours (I took from 9am to 5pm).
+
 4.Each timeblock is color coded to indicate whether it is in the past, present, or future.
+
 5.With ClickEvent user is able to enter an event into the timeblock.
+
 6.WHEN user clicks the save button for that timeblock 7.WHEN the text for that event is saved in local storage.
-8.WHEN user refreshes the page, all saved events persist.
+
+7.WHEN user refreshes the page, all saved events persist.
+
 8.WHEN user clicks trash button, timeblock is empty with deleted event.
--- script.js
+++ script.js
@@ -1,18 +1,96 @@
-// current day -> top calendar
-$('#currentDay').text(moment().format('MMMM Do YYYY, h:mm:ss a'));
+ 
+  
+  // WHEN I open the planner
+  // THEN the current day and time is displayed at the top of the calendar
+  $('#currentDay').text(moment().format('MMMM Do YYYY, h:mm:ss a'));
+ 
+  // WHEN I scroll down
+  // THEN I am presented with timeblocks for standard business hours
+  // WHEN I view the timeblocks for that day
+  // THEN each timeblock is color coded to indicate whether it is in the past (gray), present (red), or future (green)
+  window.onload = function() {
+    colorCode();
+ }
+  var now = new Date().getHours();
 
-//window.onload = function(){
-    //colorCode();
-//}
-
-// current day -> top calendar
+  function colorCode(){
+    console.log('past')
+    if (now > 9) {
+    $('#9am').addClass('past');
+  } else if (now >= 9 && now < 10) {
+    $('#9am').addClass('present');
+  } else if (now < 9) {
+    $('#9am').addClass('future');
+  }
+  if (now > 10) {
+    $('#10am').addClass('past');
+  } else if (now >= 10 && now < 11) {
+    $('#10am').addClass('present');
+  } else if (now < 11) {
+    $('#10am').addClass('future');
+  }
+  if (now > 11) {
+    $('#11am').addClass('past');
+  } else if (now >= 11 && now < 12) {
+    $('#11am').addClass('present');
+  } else if (now < 11) {
+    $('#11am').addClass('future');
+  }
+  if (now > 12) {
+    $('#12pm').addClass('past');
+  } else if (now >= 12 && now < 13) {
+    $('#12pm').addClass('present');
+  } else if (now < 12) {
+    $('#12pm').addClass('future');
+  }
+  if (now > 13) {
+    $('#1pm').addClass('past');
+  } else if (now >= 13 && now < 14) {
+    $('#1pm').addClass('present');
+  } else if (now < 13) {
+    $('#1pm').addClass('future');
+  }
+  if (now > 14) {
+    $('#2pm').addClass('past');
+  } else if (now >= 14 && now < 15) {
+    $('#2pm').addClass('present');
+  } else if (now < 14) {
+    $('#2pm').addClass('future');
+  }
+  if (now > 15) {
+    $('#3pm').addClass('past');
+  } else if (now >= 15 && now < 16) {
+    $('#3pm').addClass('present');
+  } else if (now < 15) {
+    $('#3pm').addClass('future');
+  }
+  if (now > 16) {
+    $('#4pm').addClass('past');
+  } else if (now >= 16 && now < 17) {
+    $('#4pm').addClass('present');
+  } else if (now < 16) {
+    $('#4pm').addClass('future');
+  }
+  if (now > 17) {
+    $('#5pm').addClass('past');
+  } else if (now >= 17 && now < 19) {
+    $('#5pm').addClass('present');
+  } else if (now < 17) {
+    $('#5pm').addClass('future');
+  }
+} 
 
 //button 
 // get a reference to all of the save buttons $('.saveBtn')
-//01 - ins _jQuery-elements
-
 // add a click handler to each save Button using .on() method
-//jquery click event
+$('.saveBtn').on('click', handleSave)
+function handleSave (event) {
+  console.log('Saved')
+  var value = $(this).siblings('.textarea').val(); //.textarea
+  var key = $(this).siblings('.textarea').attr('id'); //textarea
+  localStorage.setItem(key,value);
+}
+
 
 // traverse the DOM to get the values that we need to store to the localStorage
 //jquery traversing .parent() .sibling()
@@ -29,17 +107,6 @@ $('#currentDay').text(moment().format('MMMM Do YYYY, h:mm:ss a'));
 //loop throught the timeblocks
 
 // call the colorCoding function
-//var now = new Date().getHours();
-//function colorCode() {
-
-    //if (now > 8) {
-      //  $('#8am').addClass('past');
-    //} else if (now >= 8 && now < 9) {
-     //   $('#8am').addClass('present');
-   // } else if (now < 8) {
-    //    $('#8am').addClass('future');
-   // }
-
 // get the data-hour value timeblock [i].data('hour')
 //if , else - condition we compare thr currentHOur wuth the timeBLock <, ===,
 
@@ -54,4 +121,3 @@ $('#currentDay').text(moment().format('MMMM Do YYYY, h:mm:ss a'));
 
 
 // setInterval -> 15000
-// unit4
